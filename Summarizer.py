b2cb6bd2c548a5f0a940f99458832cbcfe1e217b
Kushagra Chaturvedi <81409908+chiragChaturvedi@users.noreply.github.com>
2022-05-12 20:11:33 +0530

    Update Summarizer.py

--- Summarizer.py
+++ Summarizer.py
@@ -1,22 +1,41 @@
 class ExtractiveSummarizer():
-    def tokenize(self, text):
-        import spacy as cy
-        nlp = cy.load('en_core_web_sm')
-        doc = nlp(text)
-        rawWords = [word.text for word in doc]
-        rawSents = [sent.text for sent in doc.sents]
-        return rawWords, rawSents
+    def __init__(self, raw_text = None, file_name = None):
+        if raw_text != None:
+            self.raw_text = raw_text
+        elif file_name != None:
+            try:
+                with open(file_name, 'r', encoding='utf-8') as f:
+                    self.raw_text = f.read()
+            except FileNotFoundError:
+                self.raw_text = None
+                print('File not found')
+        else:
+            self.raw_text = None
+
+
+    def tokenize(self):
+        if self.raw_text == None:
+            return 'No text was Provided at the time of object creation to tokenize!!!!...'
+        else:
+            import spacy
+            nlp = spacy.load('en_core_web_sm')
+            text = self.raw_text
+            special_chars = ('~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '[', ']', '{', '}', ';', ':', '"', "'", ',', '<', '.', '>', '?', '/', '\\', '|', '\n')
+            for char in special_chars:
+                if char in text:
+                    text = text.replace(char, ' ')
+            rawWords = [word for word in text.split()]
+            rawSents = [sent.text for sent in nlp(self.raw_text).sents]
+            return rawWords, rawSents
     def wordFrequency(self, rawWords):
         from spacy.lang.en.stop_words import STOP_WORDS
-        from string import punctuation   
         wordFrequency = {}
         for word in rawWords:
             if word.lower() not in STOP_WORDS:
-                if word not in punctuation:
-                    if word not in wordFrequency.keys():
-                        wordFrequency[word] = 1
-                    else:
-                        wordFrequency[word] += 1
+                if word not in wordFrequency.keys():
+                     wordFrequency[word] = 1
+                else:
+                    wordFrequency[word] += 1
         maxWordFrequency = max(wordFrequency.values())
         for word in wordFrequency.keys():
             wordFrequency[word] = wordFrequency[word]/maxWordFrequency
@@ -31,31 +50,38 @@ class ExtractiveSummarizer():
                     else:
                         sentsScore[sent] += wordFrequency[word]
         return sentsScore
-    def summarise(self, text: str, n: int) -> str:
-        from re import sub
+    def summarize(self, n: int):
+        # text = self.raw_text
         summary = ''
         # Text preprocessing.
-        text = sub("  " and "\n" and "\t",'', text)
-
+        # replace = ['\n', '  ', '\t']
+        # for rpl in replace:
+        #     if rpl in text:
+        #         text = text.replace(rpl, ' ')
         # getting words and sentences.
-        rawWords, rawSents = self.tokenize(text)
-        
+        rawWords, rawSents = self.tokenize()
+
         # Creating word Frequency dictionary.
         wordFrequency = self.wordFrequency(rawWords)
-        
+
         # Scoring sentences as per most frequent words.
         sentScore = self.sentScore(rawSents, wordFrequency)
-        
+
         # sorting sentences as per their scores.
-        finalSents = [keys for keys in sorted(sentScore, key = sentScore.get,reverse = True)] 
-        
+        finalSents = [keys for keys in sorted(sentScore, key = sentScore.get,reverse = True)]
+
         # creating n line of summary
         try:
             for sent in rawSents:
                 if sent in finalSents[:n]:
-                    summary += f'{sent}\n\n'
+                    summary += f'{sent.strip()}\n'
         except:
             print('Enter proper value for n')
+
+        # Writing to a file
+        with open('summary.txt', 'w') as f:
+            f.write(summary)
+
         return summary
 
 if __name__ == '__main__':
@@ -65,5 +91,17 @@ if __name__ == '__main__':
 
             If that assertion was true, this essay would be very short. All I would need to explain is why you, I and most everybody else is so convinced that we have feelings at all. If I have a tooth abscess, however, a sophisticated argument to persuade me that my pain is delusional will not lessen its torment one iota. As I have very little sympathy for this desperate solution to the mind-body problem, I shall move on.
             The majority of scholars accept consciousness as a given and seek to understand its relationship to the objective world described by science. More than a quarter of a century ago Francis Crick and I decided to set aside philosophical discussions on consciousness (which have engaged scholars since at least the time of Aristotle) and instead search for its physical footprints. What is it about a highly excitable piece of brain matter that gives rise to consciousness? Once we can understand that, we hope to get closer to solving the more fundamental problem.'''
-    sum = ExtractiveSummarizer()
-    print(sum.summarise(str, 3))        
+    sum = ExtractiveSummarizer(raw_text=str)
+    print(sum.summarize(4))
+
+
+
+
+
+
+
+
+
+
+
+
